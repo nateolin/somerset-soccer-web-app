8c370646c9a03215617fc60c402d54f5388d681b
thenolin <nateolin17@gmail.com>
2022-01-18 15:14:26 -0600

    fun setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
-import ssc-logo from "";
-
-const headingLinks = {
-  padding: "1rem",
-};
+import sscLogo from "./assets/sscLogo.jpg";
+import Typography from "@mui/material/Typography";
 
 export default function App() {
   return (
     <div style={{ display: "flex", alignItems: "baseline" }}>
       {/* <h1 style={{ justifyContent: "flex-start" }}>Somerset Soccer Club</h1> */}
-      <img src="./assets/ssc-logo.jpg" alt="Somerset Soccer Logo"></img>
+      <img src={sscLogo} alt="Somerset Soccer Logo"></img>
       <nav
         style={{
           paddingBottom: "1rem",
           justifyContent: "flex-end",
         }}
       >
-        <Link style={{ padding: "1rem" }} to="/about">
-          About Us
-        </Link>
+        <Typography>
+          <Link style={{ padding: "1rem" }} to="/about">
+            About Us
+          </Link>
+        </Typography>
         <Link style={{ padding: "1rem" }} to="/rec">
           Rec Soccer
         </Link>
